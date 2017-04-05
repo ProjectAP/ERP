9b75e9d2f6bb8bbdbecb138e290036a3a5b95972
wedaAP <weda.dewa@argopantes.com>
2017-04-05 12:07:05 +0700

    setting metronic theme for blank page.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,20 @@
 @extends('layouts.app')
+
+@section('page-title')
+    Classic Page Head
+    <small>classic page head option</small>
+@endsection
+@section('page-breadcrumb')
+    <li>
+        <a href="index.html">Home</a>
+        <i class="fa fa-angle-right"></i>
+    </li>
+    <li>
+        <span>Page Layouts</span>
+    </li>
+@endsection
+@section('page-content')
+    <div class="note note-info">
+        <p> Classic page head with page title, breadcrumbs and action dropdown menu. </p>
+    </div>
+@endsection
