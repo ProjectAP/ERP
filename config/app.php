7798decf66622cdb21a875cde3515bf7fe979b64
weda90 <weda.dewa@yahoo.co.id>
2017-04-09 12:39:00 +0700

    add package entrust, dingo, & jwt. config provider and alias in app

--- config/app.php
+++ config/app.php
@@ -155,6 +155,16 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        /*
+         * Entrust Service Providers...
+         */
+        Zizaco\Entrust\EntrustServiceProvider::class,
+        /*
+         * Dingo Service Providers...
+         */
+        Dingo\Api\Provider\LaravelServiceProvider::class,
+
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -201,6 +211,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Entrust'   => Zizaco\Entrust\EntrustFacade::class,
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class,
+
 
     ],
 
