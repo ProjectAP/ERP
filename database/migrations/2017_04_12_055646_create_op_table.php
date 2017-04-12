ae0b7a62bf5948afff9d6e8b178f2518400b9760
wedaAP <weda.dewa@argopantes.com>
2017-04-12 12:59:25 +0700

    change migration op_table no fix.

---
+++ database/migrations/2017_04_12_055646_create_op_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOpTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('op_table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('no_op');
+            $table->string('no_mp');
+            $table->string('fabric');
+            $table->string('buyer');
+            $table->string('agent');
+            $table->string('end_buyer');
+            $table->text('notes');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('op_table');
+    }
+}
