3298ebcfd37209f557fba1cfb4c42a5d38805329
wedaAP <weda.dewa@argopantes.com>
2017-04-13 08:09:59 +0700

    FIX Seeder creating.

--- database/migrations/2017_04_12_055646_create_op_table.php
+++ database/migrations/2017_04_12_055646_create_op_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOpTable extends Migration
+class CreateOPTable extends Migration
 {
     /**
      * Run the migrations.
