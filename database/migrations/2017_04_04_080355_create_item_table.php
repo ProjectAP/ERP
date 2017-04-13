ebaffb185d881d99b5c0f3c721a6526f1f9b1efd
wedaAP <weda.dewa@argopantes.com>
2017-04-13 10:25:48 +0700

    update tabel item

--- database/migrations/2017_04_04_080355_create_item_table.php
+++ database/migrations/2017_04_04_080355_create_item_table.php
@@ -14,14 +14,31 @@ class CreateItemTable extends Migration
     {
         Schema::create('tbl_item', function (Blueprint $table) {
             $table->string('code');
-            $table->string('construct_gr');
-            $table->string('construct_fin');
-            $table->string('composition');
-            $table->string('length');
-            $table->integer('type');
-            $table->integer('weave');
+            $table->string('type');
+            $table->string('weave');
+            $table->string('tp_lusi');
+            $table->string('no_lusi');
+            $table->string('tp_pakan');
+            $table->string('no_pakan');
+            $table->string('cotton');
+            $table->string('poly');
+            $table->string('dst_lusi');
+            $table->string('dst_pakan');
+            $table->string('l_greige');
+            $table->string('dst_lusi_1');
+            $table->string('dst_pakan_1');
+            $table->string('l_finish');
+            $table->string('gram');
+            $table->string('oz');
+            // $table->string('construct_gr');
+            // $table->string('construct_fin');
+            // $table->string('composition');
+            // $table->string('length');
+            // $table->integer('type');
+            // $table->integer('weave');
             $table->timestamps();
         });
+
         Schema::create('tbl_item_type', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
