bfef08c0fed614156ad5cc0ac038158d8ced2e41
wedaAP <weda.dewa@argopantes.com>
2017-04-20 14:45:36 +0700

    modified from.blade

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -52,167 +52,167 @@
 	<div class="row" id="form-data">
 		<div class="col-md-12 form">
 			<!-- BEGIN FORM-->
-                    <form action="#" class="horizontal-form">
-                        <div class="form-body">
-                            <h3 class="form-section">Item Info</h3>
-                            <!--/row-->
-                            <div class="row">
-                                <div class="col-md-2">
-                                    <div class="form-group">
-                                        <label class="control-label">Fabric</label>
-                                        <select class="form-control" name="fabric">
-                                            <option value="">GR</option>
-                                            <option value="">YD</option>
-                                        </select>
-                                    </div>
-                                </div>
-                            </div>
-                            <!--/row-->
-                            <div class="row">
-	                            <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">Type</label>
-                                        <select class="form-control" name="item_type">
-                                            <option value="">TC</option>
-                                            <option value="">CT</option>
-                                            <option value="">Other</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <!--/span-->
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">No. Item</label>
-                                        <input type="text" id="no_item" class="form-control" placeholder="No. Item">
-                                    </div>
-                                </div>
-                                <!--/span-->
-                                
-                                <!--/span-->
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">Weave</label>
-                                        <select class="form-control" name="weave">
-                                            <option value="">PLAIN</option>
-                                            <option value="">PLAT</option>
-                                            <option value="">TWILL 2/1</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <!--/span-->
-                            </div>
-                            <div class="row">
-                            	<div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">Cotton</label>
-                                        <input type="text" id="cotton" class="form-control" placeholder="Cotton">
-                                    </div>
-                                </div>
-                                <!--/span-->
-                                <div class="col-md-3">
-                                    <div class="form-group">
-                                        <label class="control-label">Polyester</label>
-                                        <input type="text" id="poly" class="form-control" placeholder="Polyester">
-                                    </div>
-                                </div>
-                                <!--/span-->
-                            </div>
-                            <h3 class="form-section">Yarn Info</h3>
-                            <!--/row-->
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Type Lusi</label>
-                                        <select class="form-control" name="type_lusi">
-                                            <option value="">TC</option>
-                                            <option value="">CVC</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <!--/span-->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">No. Lusi</label>
-                                        <input type="text" class="form-control" name="no_lusi"> 
-                                    </div>
-                                </div>
-                                <!--/span-->
-                            </div>
-                            <!--/row-->
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Type Pakan</label>
-                                        <select class="form-control" name="type_pakan">
-                                            <option value="">TC</option>
-                                            <option value="">CVC</option>
-                                        </select>
-                                    </div>
-                                </div>
-                                <!--/span-->
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">No. Pakan</label>
-                                        <input type="text" class="form-control" name="no_pakan"> 
-                                    </div>
-                                </div>
-                                <!--/span-->
-                            </div>
-                            <!--/row-->
-                            <h3 class="form-section">Construct Greige</h3>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Dst. Lusi</label>
-                                        <input type="text" class="form-control" name="dst_lusi_g"> 
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Dst. Pakan</label>
-                                        <input type="text" class="form-control" name="dst_pakan_g"> 
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Length/Width</label>
-                                        <input type="text" class="form-control" name="l_greige"> </div>
-                                </div>
-                                <!--/span-->
-                            </div>
-                            <h3 class="form-section">Construct Finish</h3>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Dst. Lusi</label>
-                                        <input type="text" class="form-control" name="dst_lusi_f"> 
-                                    </div>
-                                </div>
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label class="control-label">Dst. Pakan</label>
-                                        <input type="text" class="form-control" name="dst_pakan_f"> 
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-6">
-                                    <div class="form-group">
-                                        <label>Length/Width</label>
-                                        <input type="text" class="form-control" name="l_finish"> </div>
-                                </div>
-                                <!--/span-->
+            <form action="#" class="horizontal-form">
+                <div class="form-body">
+                    <h3 class="form-section">Item Info</h3>
+                    <!--/row-->
+                    <div class="row">
+                        <div class="col-md-2">
+                            <div class="form-group">
+                                <label class="control-label">Fabric</label>
+                                <select class="form-control" name="fabric">
+                                    <option value="">GR</option>
+                                    <option value="">YD</option>
+                                </select>
                             </div>
                         </div>
-                        <div class="form-actions right">
-                            <button type="button" class="btn default">Cancel</button>
-                            <button type="submit" class="btn blue">
-                                <i class="fa fa-check"></i> Save</button>
+                    </div>
+                    <!--/row-->
+                    <div class="row">
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Type</label>
+                                <select class="form-control" name="item_type">
+                                    <option value="">TC</option>
+                                    <option value="">CT</option>
+                                    <option value="">Other</option>
+                                </select>
+                            </div>
                         </div>
-                    </form>
-                    <!-- END FORM-->
+                        <!--/span-->
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">No. Item</label>
+                                <input type="text" id="no_item" class="form-control" placeholder="No. Item">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        
+                        <!--/span-->
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Weave</label>
+                                <select class="form-control" name="weave">
+                                    <option value="">PLAIN</option>
+                                    <option value="">PLAT</option>
+                                    <option value="">TWILL 2/1</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                    <div class="row">
+                    	<div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Cotton</label>
+                                <input type="text" id="cotton" class="form-control" placeholder="Cotton">
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="col-md-3">
+                            <div class="form-group">
+                                <label class="control-label">Polyester</label>
+                                <input type="text" id="poly" class="form-control" placeholder="Polyester">
+                            </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                    <h3 class="form-section">Yarn Info</h3>
+                    <!--/row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Type Lusi</label>
+                                <select class="form-control" name="type_lusi">
+                                    <option value="">TC</option>
+                                    <option value="">CVC</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">No. Lusi</label>
+                                <input type="text" class="form-control" name="no_lusi"> 
+                            </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                    <!--/row-->
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Type Pakan</label>
+                                <select class="form-control" name="type_pakan">
+                                    <option value="">TC</option>
+                                    <option value="">CVC</option>
+                                </select>
+                            </div>
+                        </div>
+                        <!--/span-->
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">No. Pakan</label>
+                                <input type="text" class="form-control" name="no_pakan"> 
+                            </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                    <!--/row-->
+                    <h3 class="form-section">Construct Greige</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Dst. Lusi</label>
+                                <input type="text" class="form-control" name="dst_lusi_g"> 
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Dst. Pakan</label>
+                                <input type="text" class="form-control" name="dst_pakan_g"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Length/Width</label>
+                                <input type="text" class="form-control" name="l_greige"> </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                    <h3 class="form-section">Construct Finish</h3>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Dst. Lusi</label>
+                                <input type="text" class="form-control" name="dst_lusi_f"> 
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label class="control-label">Dst. Pakan</label>
+                                <input type="text" class="form-control" name="dst_pakan_f"> 
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label>Length/Width</label>
+                                <input type="text" class="form-control" name="l_finish"> </div>
+                        </div>
+                        <!--/span-->
+                    </div>
+                </div>
+                <div class="form-actions right">
+                    <button type="button" class="btn default">Cancel</button>
+                    <button type="submit" class="btn blue">
+                        <i class="fa fa-check"></i> Save</button>
+                </div>
+            </form>
+            <!-- END FORM-->
 		</div>
 	</div>
 	<div class="row" id="table-data">
